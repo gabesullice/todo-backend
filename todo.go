e7155d908bf6279178b80ad346b04b5c2c095b44
Gabriel SULLICE <gabriel@sullice.com>
2015-12-28 21:53:37 -0700

    Add simple persistence with BoltDB

--- todo.go
+++ todo.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/json"
 	"flag"
+	"fmt"
+	"github.com/boltdb/bolt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/shwoodard/jsonapi"
 	"log"
@@ -9,16 +13,17 @@ import (
 	"time"
 )
 
+var db bolt.DB
 var todos []*Todo
 var serial int = 0
 
 var listen string
 
 type Todo struct {
-	Id    int    `jsonapi:"primary,todos"`
-	Title string `jsonapi:"attr,title"`
-	Body  string `jsonapi:"attr,body"`
-	Done  bool   `jsonapi:"attr,done"`
+	Id    int    `json:"id" jsonapi:"primary,todos"`
+	Title string `json:"title" jsonapi:"attr,title"`
+	Body  string `json:"body" jsonapi:"attr,body"`
+	Done  bool   `json:"done" jsonapi:"attr,done"`
 }
 
 func init() {
@@ -27,6 +32,16 @@ func init() {
 }
 
 func main() {
+	transaction(func(db *bolt.DB) error {
+		return db.Update(func(tx *bolt.Tx) error {
+			_, err := tx.CreateBucketIfNotExists([]byte("todos"))
+			if err != nil {
+				return fmt.Errorf("Bucket creation: %s", err)
+			}
+			return nil
+		})
+	})
+
 	r := httprouter.New()
 	r.HandlerFunc("POST", "/todos", Logger(AddTodo, "AddTodo"))
 	r.HandlerFunc("GET", "/todos", Logger(ListTodos, "ListTodos"))
@@ -59,26 +74,68 @@ func AddTodo(w http.ResponseWriter, r *http.Request) {
 }
 
 func ListTodos(w http.ResponseWriter, r *http.Request) {
-	todoInterface := make([]interface{}, len(todos))
-
-	for i, todo := range todos {
-		todoInterface[i] = todo
+	todos, err := getTodos()
+	if err != nil {
+		log.Printf("Getting todos: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
 	w.WriteHeader(http.StatusFound)
 	w.Header().Set("Content-Type", "application/vnd.api+json")
 
-	if err := jsonapi.MarshalManyPayload(w, todoInterface); err != nil {
+	if err := jsonapi.MarshalManyPayload(w, todos); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
 
+func getTodos() ([]interface{}, error) {
+	todos := make([]interface{}, 0)
+
+	err := transaction(func(db *bolt.DB) error {
+		return db.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte("todos"))
+			c := b.Cursor()
+
+			for k, v := c.First(); k != nil; k, v = c.Next() {
+				todo := &Todo{}
+				if err := json.Unmarshal(v, todo); err != nil {
+					return err
+				}
+				todos = append(todos, todo)
+			}
+
+			return nil
+		})
+	})
+
+	return todos, err
+}
+
 func saveTodo(todo *Todo) error {
-	todo.Id = serial
-	serial++
-	todos = append(todos, todo)
-	return nil
+	return transaction(func(db *bolt.DB) error {
+		return db.Update(func(tx *bolt.Tx) error {
+			// Retrieve the users bucket.
+			// This should be created when the DB is first opened.
+			b := tx.Bucket([]byte("todos"))
+
+			// Generate ID for the user.
+			// This returns an error only if the Tx is closed or not writeable.
+			// That can't happen in an Update() call so I ignore the error check.
+			id, _ := b.NextSequence()
+			todo.Id = int(id)
+
+			// Marshal user data into bytes.
+			buf, err := json.Marshal(todo)
+			if err != nil {
+				return err
+			}
+
+			// Persist bytes to users bucket.
+			return b.Put(itob(todo.Id), buf)
+		})
+	})
 }
 
 func Logger(inner http.HandlerFunc, name string) http.HandlerFunc {
@@ -88,11 +145,28 @@ func Logger(inner http.HandlerFunc, name string) http.HandlerFunc {
 		inner(w, r)
 
 		log.Printf(
-			"%s\t%s\t%s\t%s",
-			r.Method,
+			"%-15s%-15s%-15s%s",
 			r.RequestURI,
+			r.Method,
 			name,
 			time.Since(start),
 		)
 	})
+}
+
+func transaction(tx func(*bolt.DB) error) error {
+	db, err := bolt.Open("todo.db", 0600, &bolt.Options{Timeout: 3 * time.Second})
+	if err != nil {
+		log.Fatal("BoltDB: ", err.Error())
+	}
+	defer db.Close()
+
+	return tx(db)
+}
+
+// itob returns an 8-byte big endian representation of v.
+func itob(v int) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(v))
+	return b
 }
