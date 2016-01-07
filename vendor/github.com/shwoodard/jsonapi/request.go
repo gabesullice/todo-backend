31b0905335967f7e38eed4a35c2b95b6d581f525
Gabriel SULLICE <gabriel@sullice.com>
2016-01-06 21:34:39 -0700

    Vendor all dependencies

---
+++ vendor/github.com/shwoodard/jsonapi/request.go
@@ -0,0 +1,323 @@
+package jsonapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Convert an io into a struct instance using jsonapi tags on struct fields.
+// Method supports single request payloads only, at the moment. Bulk creates and updates
+// are not supported yet.
+//
+// Will Unmarshal embedded and sideloaded payloads.  The latter is only possible if the
+// object graph is complete.  That is, in the "relationships" data there are type and id,
+// keys that correspond to records in the "included" array.
+//
+// For example you could pass it, in, req.Body and, model, a BlogPost
+// struct instance to populate in an http handler,
+//
+//   func CreateBlog(w http.ResponseWriter, r *http.Request) {
+//   	blog := new(Blog)
+//
+//   	if err := jsonapi.UnmarshalPayload(r.Body, blog); err != nil {
+//   		http.Error(w, err.Error(), 500)
+//   		return
+//   	}
+//
+//   	// ...do stuff with your blog...
+//
+//   	w.WriteHeader(201)
+//   	w.Header().Set("Content-Type", "application/vnd.api+json")
+//
+//   	if err := jsonapi.MarshalOnePayload(w, blog); err != nil {
+//   		http.Error(w, err.Error(), 500)
+//   	}
+//   }
+//
+//
+// Visit https://github.com/shwoodard/jsonapi#create for more info.
+//
+// model interface{} should be a pointer to a struct.
+func UnmarshalPayload(in io.Reader, model interface{}) error {
+	payload := new(OnePayload)
+
+	if err := json.NewDecoder(in).Decode(payload); err != nil {
+		return err
+	}
+
+	if payload.Included != nil {
+		includedMap := make(map[string]*Node)
+		for _, included := range payload.Included {
+			key := fmt.Sprintf("%s,%s", included.Type, included.Id)
+			includedMap[key] = included
+		}
+
+		return unmarshalNode(payload.Data, reflect.ValueOf(model), &includedMap)
+	} else {
+		return unmarshalNode(payload.Data, reflect.ValueOf(model), nil)
+	}
+
+}
+
+func UnmarshalManyPayload(in io.Reader, t reflect.Type) ([]interface{}, error) {
+	payload := new(ManyPayload)
+
+	if err := json.NewDecoder(in).Decode(payload); err != nil {
+		return nil, err
+	}
+
+	if payload.Included != nil {
+		includedMap := make(map[string]*Node)
+		for _, included := range payload.Included {
+			key := fmt.Sprintf("%s,%s", included.Type, included.Id)
+			includedMap[key] = included
+		}
+
+		var models []interface{}
+		for _, data := range payload.Data {
+			model := reflect.New(t.Elem())
+			err := unmarshalNode(data, model, &includedMap)
+			if err != nil {
+				return nil, err
+			}
+			models = append(models, model.Interface())
+		}
+
+		return models, nil
+	} else {
+		var models []interface{}
+
+		for _, data := range payload.Data {
+			model := reflect.New(t.Elem())
+			err := unmarshalNode(data, model, nil)
+			if err != nil {
+				return nil, err
+			}
+			models = append(models, model.Interface())
+		}
+
+		return models, nil
+	}
+
+}
+
+func unmarshalNode(data *Node, model reflect.Value, included *map[string]*Node) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("data is not a jsonapi representation of '%v'", model.Type())
+		}
+	}()
+
+	modelValue := model.Elem()
+	modelType := model.Type().Elem()
+
+	var er error
+
+	var i = 0
+	modelType.FieldByNameFunc(func(name string) bool {
+		if er != nil {
+			return false
+		}
+
+		fieldType := modelType.Field(i)
+		tag := fieldType.Tag.Get("jsonapi")
+		if tag == "" {
+			i += 1
+			return false
+		}
+
+		fieldValue := modelValue.Field(i)
+
+		i += 1
+
+		args := strings.Split(tag, ",")
+
+		if len(args) < 1 {
+			er = BadJSONAPIStructTag{fieldType.Name}
+			return false
+		}
+
+		annotation := args[0]
+
+		if (annotation == "client-id" && len(args) != 1) || (annotation != "client-id" && len(args) != 2) {
+			er = BadJSONAPIStructTag{fieldType.Name}
+			return false
+		}
+
+		if annotation == "primary" {
+			if data.Id == "" {
+				return false
+			}
+
+			if data.Type != args[1] {
+				er = errors.New("Trying to Unmarshal a type that does not match")
+				return false
+			}
+
+			if fieldValue.Kind() == reflect.String {
+				fieldValue.Set(reflect.ValueOf(data.Id))
+			} else if fieldValue.Kind() == reflect.Int {
+				id, err := strconv.Atoi(data.Id)
+				if err != nil {
+					er = err
+					return false
+				}
+				fieldValue.SetInt(int64(id))
+			} else {
+				er = errors.New("Unsuppored data type for primary key, not int or string")
+				return false
+			}
+		} else if annotation == "client-id" {
+			if data.ClientId == "" {
+				return false
+			}
+
+			fieldValue.Set(reflect.ValueOf(data.ClientId))
+		} else if annotation == "attr" {
+			attributes := data.Attributes
+			if attributes == nil {
+				return false
+			}
+
+			val := attributes[args[1]]
+
+			// next if the attribute was not included in the request
+			if val == nil {
+				return false
+			}
+
+			v := reflect.ValueOf(val)
+
+			if fieldValue.Type() == reflect.TypeOf(time.Time{}) {
+				var at int64
+
+				if v.Kind() == reflect.Float64 {
+					at = int64(v.Interface().(float64))
+				} else if v.Kind() == reflect.Int {
+					at = v.Int()
+				} else {
+					er = errors.New("Only numbers can be parsed as dates, unix timestamps")
+					return false
+				}
+
+				t := time.Unix(at, 0)
+
+				fieldValue.Set(reflect.ValueOf(t))
+
+				return false
+			}
+
+			if fieldValue.Type() == reflect.TypeOf([]string(nil)) {
+				values := make([]string, v.Len())
+				for i := 0; i < v.Len(); i++ {
+					values[i] = v.Index(i).Interface().(string)
+				}
+
+				fieldValue.Set(reflect.ValueOf(values))
+
+				return false
+			}
+
+			if fieldValue.Type() == reflect.TypeOf(new(time.Time)) {
+				var at int64
+
+				if v.Kind() == reflect.Float64 {
+					at = int64(v.Interface().(float64))
+				} else if v.Kind() == reflect.Int {
+					at = v.Int()
+				} else {
+					er = errors.New("Only numbers can be parsed as dates, unix timestamps")
+					return false
+				}
+
+				v := time.Unix(at, 0)
+				t := &v
+
+				fieldValue.Set(reflect.ValueOf(t))
+
+				return false
+			}
+
+			if fieldValue.Kind() == reflect.Int && v.Kind() == reflect.Float64 {
+				fieldValue.Set(reflect.ValueOf(int(v.Interface().(float64))))
+			} else {
+				fieldValue.Set(reflect.ValueOf(val))
+			}
+		} else if annotation == "relation" {
+			isSlice := fieldValue.Type().Kind() == reflect.Slice
+
+			if data.Relationships == nil || data.Relationships[args[1]] == nil {
+				return false
+			}
+
+			if isSlice {
+				relationship := new(RelationshipManyNode)
+
+				buf := bytes.NewBuffer(nil)
+
+				json.NewEncoder(buf).Encode(data.Relationships[args[1]])
+				json.NewDecoder(buf).Decode(relationship)
+
+				data := relationship.Data
+				models := reflect.New(fieldValue.Type()).Elem()
+
+				for _, n := range data {
+					m := reflect.New(fieldValue.Type().Elem().Elem())
+
+					if err := unmarshalNode(fullNode(n, included), m, included); err != nil {
+						er = err
+						return false
+					}
+
+					models = reflect.Append(models, m)
+				}
+
+				fieldValue.Set(models)
+			} else {
+				relationship := new(RelationshipOneNode)
+
+				buf := bytes.NewBuffer(nil)
+
+				json.NewEncoder(buf).Encode(data.Relationships[args[1]])
+				json.NewDecoder(buf).Decode(relationship)
+
+				m := reflect.New(fieldValue.Type().Elem())
+
+				if err := unmarshalNode(fullNode(relationship.Data, included), m, included); err != nil {
+					er = err
+					return false
+				}
+
+				fieldValue.Set(m)
+			}
+
+		} else {
+			er = errors.New(fmt.Sprintf("Unsupported jsonapi tag annotation, %s", annotation))
+		}
+
+		return false
+	})
+
+	if er != nil {
+		return er
+	}
+
+	return nil
+}
+
+func fullNode(n *Node, included *map[string]*Node) *Node {
+	includedKey := fmt.Sprintf("%s,%s", n.Type, n.Id)
+
+	if included != nil && (*included)[includedKey] != nil {
+		return (*included)[includedKey]
+	}
+
+	return n
+}
---
+++ vendor/github.com/shwoodard/jsonapi/response.go
@@ -0,0 +1,342 @@
+package jsonapi
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"reflect"
+	"strings"
+	"time"
+)
+
+type BadJSONAPIStructTag struct {
+	fieldTypeName string
+}
+
+func (e BadJSONAPIStructTag) Error() string {
+	return fmt.Sprintf("jsonapi tag, on %s, had too few arguments", e.fieldTypeName)
+}
+
+// MarshalOnePayload writes a jsonapi response with one, with related records sideloaded, into "included" array.
+// This method encodes a response for a single record only. Hence, data will be a single record rather
+// than an array of records.  If you want to serialize many records, see, MarshalManyPayload.
+//
+// See UnmarshalPayload for usage example.
+//
+// model interface{} should be a pointer to a struct.
+func MarshalOnePayload(w io.Writer, model interface{}) error {
+	payload, err := MarshalOne(model)
+	if err != nil {
+		return err
+	}
+
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// MarshalOne does the same as MarshalOnePayload except it just returns the payload
+// and doesn't write out results.
+// Useful is you use your JSON rendering library.
+func MarshalOne(model interface{}) (*OnePayload, error) {
+	rootNode, included, err := visitModelNode(model, true)
+	if err != nil {
+		return nil, err
+	}
+	payload := &OnePayload{Data: rootNode}
+
+	payload.Included = uniqueByTypeAndID(included)
+
+	return payload, nil
+}
+
+// MarshalManyPayload writes a jsonapi response with many records, with related records sideloaded, into "included" array.
+// This method encodes a response for a slice of records, hence data will be an array of
+// records rather than a single record.  To serialize a single record, see MarshalOnePayload
+//
+// For example you could pass it, w, your http.ResponseWriter, and, models, a slice of Blog
+// struct instance pointers as interface{}'s to write to the response,
+//
+//	 func ListBlogs(w http.ResponseWriter, r *http.Request) {
+//		 // ... fetch your blogs and filter, offset, limit, etc ...
+//
+//		 blogs := testBlogsForList()
+//
+//		 w.WriteHeader(200)
+//		 w.Header().Set("Content-Type", "application/vnd.api+json")
+//		 if err := jsonapi.MarshalManyPayload(w, blogs); err != nil {
+//			 http.Error(w, err.Error(), 500)
+//		 }
+//	 }
+//
+//
+// Visit https://github.com/shwoodard/jsonapi#list for more info.
+//
+// models []interface{} should be a slice of struct pointers.
+func MarshalManyPayload(w io.Writer, models []interface{}) error {
+	payload, err := MarshalMany(models)
+	if err != nil {
+		return err
+	}
+
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// MarshalMany does the same as MarshalManyPayload except it just returns the payload
+// and doesn't write out results.
+// Useful is you use your JSON rendering library.
+func MarshalMany(models []interface{}) (*ManyPayload, error) {
+	modelsValues := reflect.ValueOf(models)
+	data := make([]*Node, 0, modelsValues.Len())
+
+	var incl []*Node
+
+	for i := 0; i < modelsValues.Len(); i++ {
+		model := modelsValues.Index(i).Interface()
+
+		node, included, err := visitModelNode(model, true)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, node)
+		incl = append(incl, included...)
+	}
+
+	payload := &ManyPayload{
+		Data:     data,
+		Included: uniqueByTypeAndID(incl),
+	}
+
+	return payload, nil
+}
+
+// MarshalOnePayloadEmbedded - This method not meant to for use in implementation code, although feel
+// free.  The purpose of this method is for use in tests.  In most cases, your
+// request payloads for create will be embedded rather than sideloaded for related records.
+// This method will serialize a single struct pointer into an embedded json
+// response.  In other words, there will be no, "included", array in the json
+// all relationships will be serailized inline in the data.
+//
+// However, in tests, you may want to construct payloads to post to create methods
+// that are embedded to most closely resemble the payloads that will be produced by
+// the client.  This is what this method is intended for.
+//
+// model interface{} should be a pointer to a struct.
+func MarshalOnePayloadEmbedded(w io.Writer, model interface{}) error {
+	rootNode, _, err := visitModelNode(model, false)
+	if err != nil {
+		return err
+	}
+
+	payload := &OnePayload{Data: rootNode}
+
+	if err := json.NewEncoder(w).Encode(payload); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func visitModelNode(model interface{}, sideload bool) (*Node, []*Node, error) {
+	node := new(Node)
+
+	var er error
+	var included []*Node
+
+	modelType := reflect.TypeOf(model).Elem()
+	modelValue := reflect.ValueOf(model).Elem()
+
+	var i = 0
+	modelType.FieldByNameFunc(func(name string) bool {
+		if er != nil {
+			return false
+		}
+
+		structField := modelType.Field(i)
+		tag := structField.Tag.Get("jsonapi")
+		if tag == "" {
+			i++
+			return false
+		}
+
+		fieldValue := modelValue.Field(i)
+
+		i++
+
+		args := strings.Split(tag, ",")
+
+		if len(args) < 1 {
+			er = BadJSONAPIStructTag{structField.Name}
+			return false
+		}
+
+		annotation := args[0]
+
+		if (annotation == "client-id" && len(args) != 1) || (annotation != "client-id" && len(args) != 2) {
+			er = BadJSONAPIStructTag{structField.Name}
+			return false
+		}
+
+		if annotation == "primary" {
+			node.Id = fmt.Sprintf("%v", fieldValue.Interface())
+			node.Type = args[1]
+		} else if annotation == "client-id" {
+			node.ClientId = fieldValue.String()
+		} else if annotation == "attr" {
+			if node.Attributes == nil {
+				node.Attributes = make(map[string]interface{})
+			}
+
+			if fieldValue.Type() == reflect.TypeOf(time.Time{}) {
+				isZeroMethod := fieldValue.MethodByName("IsZero")
+				isZero := isZeroMethod.Call(make([]reflect.Value, 0))[0].Interface().(bool)
+				if isZero {
+					return false
+				}
+
+				unix := fieldValue.MethodByName("Unix")
+				val := unix.Call(make([]reflect.Value, 0))[0]
+				node.Attributes[args[1]] = val.Int()
+			} else if fieldValue.Type() == reflect.TypeOf(new(time.Time)) {
+				// A time pointer may be nil
+				t := reflect.ValueOf(fieldValue.Interface())
+				if t.IsNil() {
+					node.Attributes[args[1]] = nil
+				} else {
+					isZeroMethod := fieldValue.MethodByName("IsZero")
+					isZero := isZeroMethod.Call(make([]reflect.Value, 0))[0].Interface().(bool)
+					if isZero {
+						return false
+					}
+
+					unix := fieldValue.MethodByName("Unix")
+					val := unix.Call(make([]reflect.Value, 0))[0]
+					node.Attributes[args[1]] = val.Int()
+				}
+			} else {
+				node.Attributes[args[1]] = fieldValue.Interface()
+			}
+		} else if annotation == "relation" {
+			isSlice := fieldValue.Type().Kind() == reflect.Slice
+
+			if (isSlice && fieldValue.Len() < 1) || (!isSlice && fieldValue.IsNil()) {
+				return false
+			}
+
+			if node.Relationships == nil {
+				node.Relationships = make(map[string]interface{})
+			}
+
+			if sideload && included == nil {
+				included = make([]*Node, 0)
+			}
+
+			if isSlice {
+				relationship, incl, err := visitModelNodeRelationships(args[1], fieldValue, sideload)
+
+				if err == nil {
+					d := relationship.Data
+					if sideload {
+						included = append(included, incl...)
+						var shallowNodes []*Node
+						for _, node := range d {
+							shallowNodes = append(shallowNodes, toShallowNode(node))
+						}
+
+						node.Relationships[args[1]] = &RelationshipManyNode{Data: shallowNodes}
+					} else {
+						node.Relationships[args[1]] = relationship
+					}
+				} else {
+					er = err
+					return false
+				}
+			} else {
+				relationship, incl, err := visitModelNode(fieldValue.Interface(), sideload)
+				if err == nil {
+					if sideload {
+						included = append(included, incl...)
+						included = append(included, relationship)
+						node.Relationships[args[1]] = &RelationshipOneNode{Data: toShallowNode(relationship)}
+					} else {
+						node.Relationships[args[1]] = &RelationshipOneNode{Data: relationship}
+					}
+				} else {
+					er = err
+					return false
+				}
+			}
+
+		} else {
+			er = fmt.Errorf("Unsupported jsonapi tag annotation, %s", annotation)
+			return false
+		}
+
+		return false
+	})
+
+	if er != nil {
+		return nil, nil, er
+	}
+
+	return node, included, nil
+}
+
+func toShallowNode(node *Node) *Node {
+	return &Node{
+		Id:   node.Id,
+		Type: node.Type,
+	}
+}
+
+func visitModelNodeRelationships(relationName string, models reflect.Value, sideload bool) (*RelationshipManyNode, []*Node, error) {
+	var nodes []*Node
+	var included []*Node
+
+	if sideload {
+		included = make([]*Node, 0)
+	}
+
+	if models.Len() == 0 {
+		nodes = make([]*Node, 0)
+	}
+
+	for i := 0; i < models.Len(); i++ {
+		node, incl, err := visitModelNode(models.Index(i).Interface(), sideload)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		nodes = append(nodes, node)
+		included = append(included, incl...)
+	}
+
+	included = append(included, nodes...)
+
+	n := &RelationshipManyNode{Data: nodes}
+
+	return n, included, nil
+}
+
+func uniqueByTypeAndID(nodes []*Node) []*Node {
+	uniqueIncluded := make(map[string]*Node)
+
+	for i := 0; i < len(nodes); i++ {
+		n := nodes[i]
+		k := fmt.Sprintf("%s,%s", n.Type, n.Id)
+		if uniqueIncluded[k] == nil {
+			uniqueIncluded[k] = n
+		} else {
+			nodes = append(nodes[:i], nodes[i+1:]...)
+			i--
+		}
+	}
+
+	return nodes
+}
